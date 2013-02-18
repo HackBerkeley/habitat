2c25e93a376c6ff2bb824b2c5adbe967fd287502
Achal <achalddave@live.com>
2013-02-17 23:05:51 -0800

    improved js code to prevent repetitive dom refs

--- public/js/index.js
+++ public/js/index.js
@@ -10,7 +10,6 @@ function easeGrid(id, direction, maxtime) {
 /* transition between pages */
 function transition(dest) {
 	var pagefade_delay = 0;
-	console.log(dest);
 	
 	$(".grid").each(function(i, e) {
 		easeGrid($(this).attr('id'), "out", gridfade_t);
@@ -36,45 +35,58 @@ $(function() {
 	});
 	
 	$(".link").click(function() {
-		transition((($(this).attr('class').split(' ').indexOf('external') > -1) ? '' : '/') + $(this).attr('name') + ($(this).attr('name')=="login" ? "?loc="+window.location : ""));
+    var jThis = $(this);
+		transition((
+      (jThis.hasClass('external') > -1) ? '' : '/') 
+        + 
+      jThis.attr('name') 
+        + 
+      ( jThis.attr('name')=="login" ? "?loc=" + window.location : ""));
 	});
 
-        $("a").click(function() {
-          transition($(this).attr("href"));
-        });
+  $("a").click(function() {
+    transition($(this).attr("href"));
+  });
 	
 	$(".form_input").focus(function() {
-		if (!$(this).attr('value')) {
-			$(this).attr('value', $(this).attr('data-default'));
+    var jThis = $(this);
+		if (!jThis.attr('value')) {
+			jThis.attr('value', jThis.attr('data-default'));
 		}
 	});
-        $("#nameField").keypress(function () {
-          $("#nameSaveButton").show();
-          $("#nameEditIcon").hide();
-        });
-        $("#nameSaveButton").bind("click", function () {
-          $.post("/users/me", {user: {name: $("#nameField").text()}});
-          $("#nameSaveButton").hide();
-          $("#nameEditIcon").show();
-        });
-  	$("#blurb").keypress(function () {
-	  keypress++;
-          $("#saveButton").show();
-          $("#blurbEditIcon").hide();
-        });
-        $("#saveButton").bind("click", function () {
-          $.post("/users/me", {user: {blurb: $("#blurb").text()}});
-          $("#saveButton").hide();
-          $("#blurbEditIcon").show();
-          keypress = 0;
-        });
- 	$('#blurb').focus(function() {
-          $("#saveButton").show();
-          $("#blurbEditIcon").hide();
-    	}).add(saveButton).focusout(function() {
-        if (keypress == 0 && (!$(blurb).is(':focus'))) {
-          $("#saveButton").hide();
-          $("#blurbEditIcon").show();
-        }
-    });
+  var jNameField = $('#nameField'),
+      jNameSaveButton = $('#nameSaveButton'),
+      jNameEditIcon = $('#nameEditIcon'),
+      jBlurb = $('#blurb'),
+      jBlurbEditIcon = $('#blurbEditIcon'),
+      jSaveButton = $('#saveButton');
+  jNameField.keypress(function () {
+    jNameSaveButton.show();
+    jNameEditIcon.hide();
+  });
+  jNameSaveButton.bind("click", function () {
+    $.post("/users/me", {user: {name: jNameField.text()}});
+    jNameSaveButton.hide();
+    jNameEditIcon.show();
+  });
+  jBlurb.keypress(function () {
+    keypress++;
+    jSaveButton.show();
+    jBlurbEditIcon.hide();
+  });
+  jSaveButton.bind("click", function () {
+    $.post("/users/me", {user: {blurb: jBlurb.text()}});
+    jSaveButton.hide();
+    jBlurbEditIcon.show();
+    keypress = 0;
+  });
+  jBlurb.focus(function() {
+    jSaveButton.show();
+    jBlurbEditIcon.hide();
+  }).focusout(function() {
+    if (keypress == 0 && (!jBlurb.is(":focus"))) {
+      jSaveButton.hide();
+      jBlurbEditIcon.show();
+    }
+  });
 });
